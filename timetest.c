642e59cd6461275c6f883493ed227d9c27eae7a1
jamesamrundle <jamesamrundle@gmail.com>
2019-04-16 01:06:47 -0400

    2nd attempt

--- timetest.c
+++ timetest.c
@@ -11,6 +11,8 @@
 
 float stdsum , standardDeviation = 0.0;;
 
+int timeSum;
+
 long diff_time(struct timespec *t1, struct timespec *t2)
 {
 return t2->tv_nsec - t1->tv_nsec + (t2->tv_sec - t1->tv_sec) * 1000000000L;
@@ -40,60 +42,80 @@ void calculateSD(float data)
 int main(int argc, char *argv[])
 {
 
-FILE * fp;
+FILE * fp , *xx;
    /* open the file for writing*/
    fp = fopen ("./output","w");
+     xx = fopen ("./DUMP","w");
 
-char pete[5000] = {0};
+
 
 struct timespec t1, t2;
-int i ;
+int i , j ,k , r;
 int d;
 int sum;
 
-for (i = 0; i < 10000; i++) {
-    clock_gettime(CLOCK_MONOTONIC, &t1);
-sum = pete[i%5000];
-clock_gettime(CLOCK_MONOTONIC, &t2);
-d = diff_time(&t1, &t2);
+char pete[10000] = {0};
+for(i = 0;i<10000;i++){
+  pete[i] = rand()%60;
+}
 
+for (i = 1; i < 100; i++) {
+  fprintf (fp, "**************    %d     *****************  %d  ***********\n",i,i);
+for(k = 1;k<50;k++){
+  
+  r =(rand()%10000);
+fprintf(fp,"r== %d",r);
+
+for (j = 0; j < 10; j++) {
+  fprintf(fp,"arr[ %d ] ",(r +(j*i)%10000));
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+sum =+ pete[(r +(j*i)%10000)];
+clock_gettime(CLOCK_MONOTONIC, &t2);
+printf("%d\n",sum);
+d = diff_time(&t1, &t2);
+timeSum += d;
 fprintf (fp, "%d\n",d);
 // sleep(1);
 }
-
+  fprintf (fp, ">>>>>>>>>>>>>>>>spacing of %d, run time = %d \n",i,timeSum);
+timeSum = 0;
+sum = 0;
+}
+}
+fclose(xx);
 fclose(fp);
 
-int addr_cnt = 0;
-  int each;
-  int rflag;
- fp = fopen ("./output","r");
+// int addr_cnt = 0;
+//   int each;
+//   int rflag;
+//  fp = fopen ("./output","r");
 
-  if (fp != NULL) {
+//   if (fp != NULL) {
 
-      printf("hi\n");
-    rflag =fscanf(fp, "%d\n",&each);
-          addr_cnt++;
-    // printf("%x",each);
-    calculateSD(each);
+//       printf("hi\n");
+//     rflag =fscanf(fp, "%d\n",&each);
+//           addr_cnt++;
+//     // printf("%x",each);
+//     calculateSD(each);
 
-    while (rflag != EOF) {
+//     while (rflag != EOF) {
 		  
-      addr_cnt++;
-        // printf("%d",addr_cnt);
-      rflag = fscanf(fp, "%d\n",&each);
-        calculateSD(each);
+//       addr_cnt++;
+//         // printf("%d",addr_cnt);
+//       rflag = fscanf(fp, "%d\n",&each);
+//         calculateSD(each);
 
-    }
+//     }
 
    
         	
     
-  } else addr_cnt = -1;
+//   } else addr_cnt = -1;
 
-  fclose(fp);
+//   fclose(fp);
 
-    float finalStDev = sqrt(standardDeviation/10000);
-printf("final %f \n",finalStDev);
+//     float finalStDev = sqrt(standardDeviation/10000);
+// printf("final %f \n",finalStDev);
 
  
 
